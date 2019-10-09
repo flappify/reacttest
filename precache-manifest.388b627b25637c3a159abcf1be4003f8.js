336f92af4cd9510e672ac92e8998fdbf3b5f28af
Flappify <creative.flappify@gmail.com>
2019-10-09 14:48:22 +0530

    Updates

---
+++ precache-manifest.388b627b25637c3a159abcf1be4003f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98427c3276d88e89974d76d629efd39a",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "76cc9b9d69876b87a370",
+    "url": "/reacttest/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "9298f8f085822a2654c6",
+    "url": "/reacttest/static/js/2.bfee4188.chunk.js"
+  },
+  {
+    "revision": "76cc9b9d69876b87a370",
+    "url": "/reacttest/static/js/main.3aaa33d6.chunk.js"
+  },
+  {
+    "revision": "5ca3f818158eb99472e4",
+    "url": "/reacttest/static/js/runtime-main.48203e08.js"
+  }
+]);
